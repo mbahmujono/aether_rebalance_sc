df12fd03407546018d6be85823e98237d75ba1fb
dr. Diaz Adi Pradana <95735464+mbahmujono@users.noreply.github.com>
2025-06-15 04:40:41 +0000

    usdc on arbitrum fixed

--- script/_setup_script.py
+++ script/_setup_script.py
@@ -4,7 +4,7 @@ from boa.contracts.abi.abi_contract import ABIContract
 from typing import Tuple
 
 STARTING_ETH_BALANCE = int(1000e18)
-STARTING_USDC_BALANCE = int(1000e6)
+STARTING_USDC_BALANCE = int(123456e6)
 STARTING_WETH_BALANCE = int(13.5e18)
 
 
@@ -17,12 +17,23 @@ def _add_token_balance(usdc: ABIContract, weth: ABIContract, active_network: Net
     print(f'wallet address: {my_address}')
     print(f"Adding {STARTING_USDC_BALANCE} USDC and {STARTING_WETH_BALANCE} WETH to {my_address}")
     # Ensure the USDC contract is set up correctly
-    with boa.env.prank(usdc.owner()):
-        usdc.updateMasterMinter(my_address)
-    usdc.configureMinter(my_address, STARTING_USDC_BALANCE)
-    usdc.mint(my_address, STARTING_USDC_BALANCE)
-    print(f"Added {STARTING_USDC_BALANCE} USDC to {my_address}")
-    
+    try:  # Mainnet
+        # On mainnet, we need to set the master minter for USDC
+        print(f"active network chain id is {active_network.chain_id}")
+        with boa.env.prank(usdc.owner()):
+            usdc.updateMasterMinter(my_address)
+        usdc.configureMinter(my_address, STARTING_USDC_BALANCE)
+        usdc.mint(my_address, STARTING_USDC_BALANCE)
+        print(f"Added {STARTING_USDC_BALANCE} USDC to {my_address}")
+    except Exception as e:
+        print(f"Error occurred while adding USDC: {e}")
+        usdc_whale = "0x1eED63EfBA5f81D95bfe37d82C8E736b974F477b"
+        # Use prank to impersonate the whale
+        with boa.env.prank(usdc_whale):
+            usdc.sendValue(boa.env.eoa, STARTING_USDC_BALANCE)
+            print(f"Transferred {STARTING_USDC_BALANCE} USDC from whale {usdc_whale} to {my_address}")
+            print(f"USDC balance after transfer: {usdc.balanceOf(my_address) / 1e6} USDC")
+
     #deposit weth
     weth.deposit(value=STARTING_WETH_BALANCE)
     print(f"Deposited {STARTING_WETH_BALANCE} WETH to {my_address}")
@@ -50,24 +61,26 @@ def setup_script() -> Tuple[ABIContract, ABIContract, ABIContract, ABIContract]:
     starting_usdc_balance = usdc.balanceOf(boa.env.eoa)
     starting_weth_balance = weth.balanceOf(boa.env.eoa)
 
-    print(f"Starting USDC balance: {starting_usdc_balance}")
-    print(f"Starting WETH balance: {starting_weth_balance}")
+    print(f"Starting USDC balance: {starting_usdc_balance/ 1e6} USDC")
+    print(f"Starting WETH balance: {starting_weth_balance/ 1e18}")
     
     print("Getting atokens, this may take a while...")
-    a_tokens = aave_protocol_data_provider.getAllATokens()
-    a_usdc = None
-    a_weth = None
+    # a_tokens = aave_protocol_data_provider.getAllATokens()
+    a_usdc = active_network.manifest_named("usdc", address=0x724dc807b04555b71ed48a6896b6f41593b8c637)
+    a_weth = active_network.manifest_named("usdc", address=0xe50fA9b3c56FfB159cB0FCA61F5c9D750e8128c8)
+    # a_usdc = None
+    # a_weth = None
     print("Fetching aTokens... done")
     
-    token_prefix = "aEth" if active_network.chain_id == 1 else "aArb"
-    for a_token in a_tokens:
-        print("Found aToken:", a_token[0])
-        if a_token[0] == f"{token_prefix}USDC":
-            print(f"Found aETHUSDC/aArbUSDC at {a_token[1]}")
-            a_usdc = active_network.manifest_named("usdc", address=a_token[1])
-        if a_token[0] == f"{token_prefix}WETH":
-            print(f"Found aETHWETH/aArbWETH at {a_token[1]}")
-            a_weth = active_network.manifest_named("usdc", address=a_token[1])
+    # token_prefix = "aEth" if active_network.chain_id == 1 else "aArb"
+    # for a_token in a_tokens:
+    #     print("Found aToken:", a_token[0])
+    #     if a_token[0] == f"{token_prefix}USDC":
+    #         print(f"Found aETHUSDC/aArbUSDC at {a_token[1]}")
+    #         a_usdc = active_network.manifest_named("usdc", address=a_token[1])
+    #     if a_token[0] == f"{token_prefix}WETH":
+    #         print(f"Found aETHWETH/aArbWETH at {a_token[1]}")
+    #         a_weth = active_network.manifest_named("usdc", address=a_token[1])
             
     starting_usdc_balance = usdc.balanceOf(boa.env.eoa)
     starting_weth_balance = weth.balanceOf(boa.env.eoa)
